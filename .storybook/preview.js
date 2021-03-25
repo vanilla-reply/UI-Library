3346718d7e71f886e5c619a2ff9d330d9f565013
Hauke Brinkmann <h.brinkmann@reply.de>
2021-03-25 10:57:35 +0000

    :wrench: add tailwind to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-
+import '../src/assets/styles/tailwind.css'
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button>{{ label }}</button>
+  <button class="border p-4">{{ label }}</button>
 </template>
 
 <script>
