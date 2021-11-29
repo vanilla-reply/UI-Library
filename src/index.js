c308c607dabb044015205c0d2f03c579bbf7a8e2
Florenz Heldermann <hi@flore.nz>
2021-11-29 17:14:19 +0100

    :construction: stuff

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 export { default as UiButton } from './components/UiButton.vue'
+export { default as UiBrand } from './components/UiBrand.vue'
+export { default as UiDropdown } from './components/UiDropdown.vue'
--- vite.config.js
+++ vite.config.js
@@ -7,31 +7,38 @@ export default defineConfig({
   plugins: [vue()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/main.js'),
+      entry: path.resolve(__dirname, 'src/index.js'),
       name: 'PttUiLib',
-      fileName: (format) => `ptt-ui-lib.${format}.js`
+      // fileName: (format) => `ptt-ui-lib.${format}.js`
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
       external: ['vue'],
-      output: [{
-        format: "esm",
-        sourcemap: true,
-        esModule: true,
-        exports: "named",
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
         globals: {
-          vue: "Vue"
-        }
-      }, {
-        format: "umd",
-        inlineDynamicImports: true,
-        interop: "esModule",
-        exports: "named",
-        globals: {
-          vue: "Vue"
-        }
-      }],
+          vue: 'Vue',
+        },
+      },
+      // output: [{
+      //   format: "esm",
+      //   sourcemap: true,
+      //   esModule: true,
+      //   exports: "named",
+      //   globals: {
+      //     vue: "Vue"
+      //   }
+      // }, {
+      //   format: "umd",
+      //   inlineDynamicImports: true,
+      //   interop: "esModule",
+      //   exports: "named",
+      //   globals: {
+      //     vue: "Vue"
+      //   }
+      // }],
     }
   }
 })
