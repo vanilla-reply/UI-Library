ff9543e48ed6adf555773eba5694b08e67088d0e
Florenz Heldermann <hi@flore.nz>
2021-11-30 16:57:05 +0100

    :sparkles: add hero component

--- src/components/UiHero.stories.js
+++ src/components/UiHero.stories.js
@@ -1,7 +1,7 @@
 import UiHero from "./UiHero.vue";
 
 export default {
-  title: "Core/Hero",
+  title: "Component/Hero",
   component: UiHero,
 }
 
@@ -16,6 +16,24 @@ const Template = (args) => ({
   export const Hero = Template.bind({});
   Hero.args = {
     headline: "Headline",
-    introduction: "Introduction",
+    headlineElement: "h3",
+    subheadline: "Introduction",
+    subheadlineElement: "h4",
     text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    ctas: [
+      {
+        variant: 'primary',
+        size: 'lg',
+        href: '#',
+        title: 'title',
+        text: 'CTA 1'
+      },
+      {
+        variant: 'secondary',
+        size: 'lg',
+        href: '#',
+        title: 'title2',
+        text: 'CTA 2'
+      }
+    ]
   }
--- src/components/UiHero.vue
+++ src/components/UiHero.vue
@@ -1,36 +1,73 @@
 <template>
-  <ui-container>
-    <div class="grid grid-cols-3">
-      <div class="col-span-2">
-          <h2>{{ headline }}</h2>
-          <h3>{{ introduction }}</h3>
-          <p>{{ text }}</p>
-        </div>
-      <div></div>
+  <div class="ui-hero">
+    <div class="ui-hero__inner">
+      <component :is="headlineElement" class="ui-hero__headline">
+        {{ headline }}
+      </component>
+      <component :is="subheadlineElement" class="ui-hero__subheadline">
+        {{ subheadline }}
+      </component>
+      <p>{{ text }}</p>
+      <div v-if="ctas.length" class="ui-hero__cta-bar">
+        <ui-button
+          v-for="(item, index) in ctas"
+          :key="index"
+          :variant="item.variant"
+          :size="item.arianize"
+          :href="item.href"
+          :title="item.title"
+          :text="item.text"
+        >
+          {{ item.text }}
+        </ui-button>
+      </div>
     </div>
-  </ui-container>
+  </div>
 </template>
 
-import Container from "./UiContainer";
-
 <script>
-import UiContainer from './UiContainer.vue';
+import UiButton from "./UiButton.vue";
 export default {
-  components: { UiContainer },
+  components: { UiButton },
   name: "UiHero",
   props: {
     headline: {
       type: String,
       default: "Headline",
     },
-    introduction: {
+    headlineElement: {
+      type: String,
+      default: "h2",
+    },
+    subheadline: {
       type: String,
       default: "Introduction",
+    },
+    subheadlineElement: {
+      type: String,
+      default: "h3",
     },
     text: {
       type: String,
       default: "Lorem Ipsum dolor sit amet.",
     },
+    ctas: {
+      type: Array,
+      default: () => [],
+    },
   },
 };
 </script>
+
+<style lang="pcss">
+.ui-hero {
+  @apply font-sans;
+  @apply border border-solid border-neutral-300;
+  @apply px-8 pt-4 pb-8;
+
+  &__headline {}
+  &__subheadline {}
+  &__body {}
+  &__cta {}
+}
+</style>
